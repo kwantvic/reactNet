89dddaa84b95e65ac1ea8e025de338e9e4ac0d60
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-01-12 02:39:32 +0200

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://banner2.cleanpng.com/20180513/fpq/kisspng-royalty-free-logo-dragon-5af7c7407136a9.1946411915261878404637.jpg" />
+    </header>
+}
+
+export default Header;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg" />
+        </div>
+        <div>
+            ava + description
+    </div>
+        <div>
+            My post
+        <div>
+                New post
+        </div>
+            <div>
+                <div>
+                    post 1
+            </div>
+                <div>
+                    post 2
+            </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
+
