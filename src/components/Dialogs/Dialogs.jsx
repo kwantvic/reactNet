73320fd09e5835fad17ea27077bed105d3357316
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-01-21 19:44:46 +0200

    u21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-// import style from './Dialogs.module.css'
+import style from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    <NavLink to="/dialogs/1">Dimych</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/2">Victor</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/3">Petya</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/4">Olya</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/5">Sveta</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to="/dialogs/6">Augustin</NavLink>
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>Yo!</div>
+            </div>
         </div>
     )
 }
