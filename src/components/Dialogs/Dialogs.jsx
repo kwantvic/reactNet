bbb816d874dcf5e85de4403cf0ef63ee1ad01f80
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-02-04 01:31:44 +0200

    u28

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,26 +3,11 @@ import style from './Dialogs.module.css';
 import DialogsItem from './DialogItem/DilogItem';
 import Message from './Message/Message';
 
-const Dialogs = () => {
+const Dialogs = (props) => {
 
-    let dialogs = [
-        { id: 1, name: "Dimich" },
-        { id: 2, name: "Victor" },
-        { id: 3, name: "Petya" },
-        { id: 4, name: "Olya" },
-        { id: 5, name: "Sveta" },
-        { id: 6, name: "Augustin" }
-    ]
+    let dialogsElements = props.dialogs.map(d => <DialogsItem name={d.name} id={d.id} />);
 
-    let messages = [
-        { id: 1, textMessage: "Hi" },
-        { id: 2, textMessage: "How are you?" },
-        { id: 3, textMessage: "Yo!" }
-    ]
-
-    let dialogsElements = dialogs.map(d => <DialogsItem name={d.name} id={d.id} />);
-
-    let messagesElements = messages.map(m => <Message textMessage={m.textMessage} />);
+    let messagesElements = props.messages.map(m => <Message textMessage={m.textMessage} />);
 
     return (
         <div className={style.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import style from './MyPosts.module.css';
 import Post from './Posts/Post';
 
-const MyPosts = () => {
+const MyPosts = (props) => {
 
-    let posts = [
-        { id: 1, message: "Hi, how are you?", likesCount: 5 },
-        { id: 2, message: "It's my first post!!!", likesCount: 16 }
-    ]
-
-    let postsElements = posts.map(p => <Post messege={p.message} likesCount={p.likesCount} />)
+    let postsElements = props.posts.map(p => <Post messege={p.message} likesCount={p.likesCount} />)
 
     return (
         <div className={style.postsBlock}>
