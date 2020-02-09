16688c5ab14bf51fdaa4e394298b2fbfe11d4f8d
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-02-09 14:16:41 +0200

    u32

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import { Route, BrowserRouter } from "react-router-dom";
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
+import { addPost } from './redux/state';
 
 
 const App = (props) => {
-	return (		
-			<div className='app-wrapper'>
-				<Header />
-				<Navbar />
-				<div className='app-wrapper-content'>
-					<Route path='/profile' render={() => (<Profile state={props.state.profilePage}/>)} />
-					<Route path='/dialogs' render={() => (<Dialogs state={props.state.dialogsPage}/>)} />
-					<Route path='/news' component={News} />
-					<Route path='/music' component={Music} />
-					<Route path='/settings' component={Settings} />
-				</div>
+	return (
+		<div className='app-wrapper'>
+			<Header />
+			<Navbar />
+			<div className='app-wrapper-content'>
+				<Route path='/profile' render={() => (<Profile
+					state={props.state.profilePage}
+					addPost={props.addPost} />)} />
+				<Route path='/dialogs' render={() => (<Dialogs state={props.state.dialogsPage} />)} />
+				<Route path='/news' component={News} />
+				<Route path='/music' component={Music} />
+				<Route path='/settings' component={Settings} />
 			</div>
+		</div>
 	);
 }
 export default App;
