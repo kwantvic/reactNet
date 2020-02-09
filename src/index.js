f858f27f413797dbb63821aa1d1d3c14cb9a2e33
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-02-09 18:06:39 +0200

    u33

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { renderEntireTree } from './render';
 import state from './redux/state';
-import {addPost} from './redux/state';
-import { Route, BrowserRouter } from "react-router-dom";
 
-// addPost('FSDDDDDDDDDDDDDDDDDDDDDDDDDDDDD');
+renderEntireTree(state);
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App state={state} addPost={addPost} />
-    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { renderEntireTree } from "../render";
+
 let state = {
     profilePage: {
         posts: [
@@ -24,13 +26,13 @@ let state = {
 }
 
 export let addPost = (postMessage) => {
-    debugger;
     let newPost = {
         id: 5,
         message: postMessage,
         likesCount: 0
     };
     state.profilePage.posts.push(newPost);
+    renderEntireTree(state);
 }
 
 export default state;
