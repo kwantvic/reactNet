25d26a64fc69e952214d171984949c902458d4aa
kwantvic <kwantvic@gmail.com>
2020-02-17 23:27:02 +0200

    u35

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { renderEntireTree } from './render';
-import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText, subscribe} from './redux/state';
+import state from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
+
+
+let renderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
 
 renderEntireTree(state);
+
+subscribe(renderEntireTree);
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { renderEntireTree } from "../render";
+let renderEntireTree = () => {
+    console.log('State changed');
+}
 
 let state = {
     profilePage: {
@@ -26,7 +28,7 @@ let state = {
     }
 }
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -37,9 +39,13 @@ export let addPost = () => {
     renderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     renderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    renderEntireTree = observer;
 }
 
 export default state;
