1e15934244b0a66df02fe02445d8ec4c983f04f5
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-03-29 12:12:50 +0300

    u52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import styles from './Users.module.css';
 import * as axios from 'axios' ;
-
-// [
-//     { id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR57Pz7X7j2pagy_Q9IlQOiYJN37F_lUHOsVMHPRBtfAtRHkgWr', followed: true, fullName: 'Dmitry', status: 'I\'m a BOSS!', location: { city: 'Minsk', country: 'Belarus' } },
-//     { id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWEj-oaqCMXbKzIDXKFY9d1AdIxI2zzP9tI5ys7gjX3WNhnX2w', followed: false, fullName: 'Victor', status: 'My palm turned into a fist.', location: { city: 'Saint Petersburg', country: 'Russia' } },
-//     { id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT-XAEAl5EN3m-j7QPn20vLv4JVDVhkooHDOjTR32sQ35BycF9S', followed: true, fullName: 'Petr', status: 'Целая жизнь дана, чтобы душу изменить.', location: { city: 'Moscow', country: 'Russia' } },
-//     { id: 4, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSkAKDPsgwoERNNyUZNWhNfKyHdC-nYG9wUi47CfUe5Y4bzWYQW', followed: false, fullName: 'Sasha', status: 'Не лгите родителям и врачам.', location: { city: 'Kiev', country: 'Ukraine' } }
-// ]
+import userIcon from '../../assets/images/userIcon.png';
 
 const Users = (props) => {
+
+let getUsers = () => {
     if (props.users.length === 0) {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             debugger;
             props.setUsers(response.data.items);
         });
     }
+}    
 
     return <div>
+        <button onClick={getUsers}>Get users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userIcon} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
