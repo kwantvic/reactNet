0623004e21e64496b7ce44bd96af40e2ed2050cd
VERS_41_2 <VERS_41_2@FED.COM.UA>
2020-05-30 16:19:53 +0300

    u63 last was u63

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "02fba007-33f8-460c-8efe-ba82565909d7"
+    }
+});
+
+export const userAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+}
+
